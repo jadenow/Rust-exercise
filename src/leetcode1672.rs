321dde3a391be715d65876d15acbd93b11c4a092
Jade <jinatpc@cluml.com>
2024-10-27 19:26:47 +0900

    Solve LeetCode problem 1672
    Implemet function to get the maximum sum of a 1D vector in a 2D vector.
    This will close #8.

---
+++ src/leetcode1672.rs
@@ -0,0 +1,18 @@
+fn solution(accounts: Vec<Vec<i32>>) -> i32 {
+    println!("1672. Richest Customer Wealth");
+
+    let ans = accounts
+        .iter()
+        .map(|person| person.iter().sum::<i32>())
+        .max()
+        .expect("Accounts vector is not empty");
+
+    ans
+}
+
+#[test]
+fn leetcode1672_case1() {
+    let accounts: Vec<Vec<i32>> = vec![vec![1, 2, 3], vec![3, 2, 1]];
+    let desired = 6;
+    assert_eq!(solution(accounts), desired);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod leetcode1480;
+mod leetcode1672;
 mod leetcode1929;
 mod leetcode2235;
 fn main() {}
